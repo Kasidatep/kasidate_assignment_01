afd9cbedce558beb975fbe0f09fedd8266a97746
Kasidate P. <bank@kasidate.me>
2025-06-08 15:00:01 +0700

    first commit

---
+++ kasidate_phlaiphueak.js
@@ -0,0 +1,34 @@
+function quickestPath(portals) {
+   const END_BLOCK = 200;
+  let queue = [{ pos: 1, steps: 0 }];  
+  let visited = []
+  visited[1] = true;
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    const position = current.pos;
+    const stepCount = current.steps;
+
+    if (position === END_BLOCK) return stepCount;
+
+    for (let move = 1; move <= 11; move++) {
+      let nextPos = position + move;
+
+      if (nextPos > END_BLOCK) continue;
+
+      for (let i = 0; i < portals.length; i++) {
+        if (portals[i].location === nextPos) {
+          nextPos = portals[i].destination;
+          break;
+        }
+      }
+
+      if (!visited[nextPos]) {
+        visited[nextPos] = true;
+        queue.push({ pos: nextPos, steps: stepCount + 1 });
+      }
+    }
+  }
+  return -1;
+}
+
